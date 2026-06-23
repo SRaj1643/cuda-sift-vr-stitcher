ba791be5246879c7a562f69d11fd819b4aed921a
Raj Yadav <ysraj1643@gmail.com>
2026-06-23 10:20:44 +0530

    Add CameraData structure to camera.h

--- include/camera.h
+++ include/camera.h
@@ -1,1 +1,17 @@
 
+#pragma once
+
+struct CameraData
+{
+    int width;
+    int height;
+
+    // Original image
+    float* image_gpu;
+
+    // Gaussian Pyramid
+    float* gaussian_gpu;
+
+    // DoG Pyramid
+    float* dog_gpu;
+};
