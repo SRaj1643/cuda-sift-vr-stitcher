1fb13f5d07c3d5cbda7e5513d0ecf349d23e1bc2
Raj Yadav <ysraj1643@gmail.com>
2026-06-25 20:29:15 +0530

    Implement MemoryManager class for CUDA operations
    Added MemoryManager class for GPU memory management.

--- src/memory_manager.cu
+++ src/memory_manager.cu
@@ -1,1 +1,22 @@
+#pragma once
 
+#include <cuda_runtime.h>
+
+class MemoryManager
+{
+public:
+
+    static float* allocateFloatArray(int size);
+
+    static void freeMemory(float* ptr);
+
+    static void copyToGPU(
+        float* gpu_ptr,
+        float* cpu_ptr,
+        int size);
+
+    static void copyToCPU(
+        float* cpu_ptr,
+        float* gpu_ptr,
+        int size);
+};
