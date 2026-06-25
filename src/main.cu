0b1a6382465af0cb62c4e2d50486cd84dfc067ed
Raj Yadav <ysraj1643@gmail.com>
2026-06-25 20:34:23 +0530

    Update main.cu

--- src/main.cu
+++ src/main.cu
@@ -2,6 +2,7 @@
 
 #include "../include/config.h"
 #include "../include/camera_manager.h"
+#include "../include/memory_manager.h"
 
 int main()
 {
@@ -9,15 +10,40 @@ int main()
 
     manager.active_cameras = 2;
 
-    std::cout << "CUDA SIFT VR Stitcher\n";
+    const int SIZE = 10;
 
-    std::cout << "Maximum Cameras Supported: "
-              << MAX_CAMERAS
-              << std::endl;
+    float cpu_data[SIZE];
 
-    std::cout << "Currently Active Cameras: "
-              << manager.active_cameras
-              << std::endl;
+    for(int i = 0; i < SIZE; i++)
+    {
+        cpu_data[i] = i;
+    }
+
+    float* gpu_data =
+        MemoryManager::allocateFloatArray(SIZE);
+
+    MemoryManager::copyToGPU(
+        gpu_data,
+        cpu_data,
+        SIZE);
+
+    float cpu_result[SIZE];
+
+    MemoryManager::copyToCPU(
+        cpu_result,
+        gpu_data,
+        SIZE);
+
+    std::cout << "Transferred Values:\n";
+
+    for(int i = 0; i < SIZE; i++)
+    {
+        std::cout << cpu_result[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    MemoryManager::freeMemory(gpu_data);
 
     return 0;
 }
