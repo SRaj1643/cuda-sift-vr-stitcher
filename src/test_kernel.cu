75aa6e081dc4bce716314812de918967728a0192
Raj Yadav <ysraj1643@gmail.com>
2026-06-27 12:13:35 +0530

    Refactor multiplyByTwoKernel and add launch function

--- src/test_kernel.cu
+++ src/test_kernel.cu
@@ -1,7 +1,11 @@
+#include "../include/test_kernel.h"
+
 #include <cuda_runtime.h>
 
 __global__
-void multiplyByTwoKernel(float* data, int size)
+void multiplyByTwoKernel(
+    float* data,
+    int size)
 {
     int idx =
         blockIdx.x * blockDim.x +
@@ -11,4 +15,20 @@ void multiplyByTwoKernel(float* data, int size)
     {
         data[idx] *= 2.0f;
     }
+}
+
+void launchMultiplyByTwo(
+    float* gpu_data,
+    int size)
+{
+    int threads = 256;
+
+    int blocks =
+        (size + threads - 1) / threads;
+
+    multiplyByTwoKernel<<<blocks, threads>>>(
+        gpu_data,
+        size);
+
+    cudaDeviceSynchronize();
 }
