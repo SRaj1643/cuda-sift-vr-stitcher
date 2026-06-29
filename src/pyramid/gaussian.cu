8c4bc4f11327ed8817143d96e5dbe8219523f787
Raj Yadav <ysraj1643@gmail.com>
2026-06-29 09:27:39 +0530

    Add Gaussian pyramid module with initialization function
    Implemented the buildGaussianPyramid function stub.

---
+++ src/pyramid/gaussian.cu
@@ -0,0 +1,14 @@
+#include "gaussian.h"
+
+#include <iostream>
+
+void buildGaussianPyramid(
+    const float* input_image,
+    int width,
+    int height,
+    int num_octaves,
+    int num_scales,
+    std::vector<float*>& gaussian_pyramid)
+{
+    std::cout << "Gaussian Pyramid Module Initialized" << std::endl;
+}
