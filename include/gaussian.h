95f9fe099a232734928b29f59e486c0d19029df7
Raj Yadav <ysraj1643@gmail.com>
2026-06-29 12:21:08 +0530

    Refactor gaussian.h to include OpenCV and new functions

--- include/gaussian.h
+++ include/gaussian.h
@@ -1,9 +1,25 @@
-#pragma once
+#ifndef GAUSSIAN_H
+#define GAUSSIAN_H
 
-void gaussianBlurCUDA(
-    const float* input,
-    float* output,
-    int width,
-    int height,
-    float sigma
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+#define NUM_OCTAVES 4
+#define NUM_SCALES 8
+
+// Same sigma values as CPU version
+extern const float SIGMAS[NUM_SCALES];
+
+// CPU reference function
+std::vector<std::vector<cv::Mat>>
+buildGaussianPyramidCPU(
+    const cv::Mat& image
 );
+
+// CUDA version
+std::vector<std::vector<cv::Mat>>
+buildGaussianPyramidCUDA(
+    const cv::Mat& image
+);
+
+#endif
