bdf14dc7f4a0ca564e49849c92f5a2a97f3473da
Raj Yadav <ysraj1643@gmail.com>
2026-06-30 21:35:09 +0530

    Add saveGaussianPyramid function declaration
    Added a function to save Gaussian pyramid images to a specified folder.

--- include/gaussian.h
+++ include/gaussian.h
@@ -3,6 +3,7 @@
 
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <string>
 
 #define NUM_OCTAVES 4
 #define NUM_SCALES 8
@@ -10,16 +11,28 @@
 // Same sigma values as CPU version
 extern const float SIGMAS[NUM_SCALES];
 
-// CPU reference function
+// ----------------------------------------------------
+// CPU Reference
+// ----------------------------------------------------
 std::vector<std::vector<cv::Mat>>
 buildGaussianPyramidCPU(
     const cv::Mat& image
 );
 
-// CUDA version
+// ----------------------------------------------------
+// CUDA Version
+// ----------------------------------------------------
 std::vector<std::vector<cv::Mat>>
 buildGaussianPyramidCUDA(
     const cv::Mat& image
+);
+
+// ----------------------------------------------------
+// Save Pyramid Images
+// ----------------------------------------------------
+void saveGaussianPyramid(
+    const std::vector<std::vector<cv::Mat>>& pyramid,
+    const std::string& outputFolder
 );
 
 #endif
